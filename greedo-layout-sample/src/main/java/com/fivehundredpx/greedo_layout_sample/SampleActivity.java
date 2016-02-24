0fa1182681efb5fb644f85b49f2c423a1d2e3caf
Julian Villella <julian.villella@hotmail.com>
2016-02-24 14:48:35 -0500

    Add boilerplate sample project files

--- greedo-layout-sample/src/main/java/com/fivehundredpx/greedo_layout_sample/SampleActivity.java
+++ greedo-layout-sample/src/main/java/com/fivehundredpx/greedo_layout_sample/SampleActivity.java
@@ -9,5 +9,7 @@ public class SampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
+
+
     }
 }
