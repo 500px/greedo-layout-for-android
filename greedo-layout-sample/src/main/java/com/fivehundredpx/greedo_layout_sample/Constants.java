1d962a06190ab4bad934221ab88b11f505f9b28b
Julian Villella <julian.villella@hotmail.com>
2016-03-02 13:19:09 -0500

    Use larger memory cache and remove disk cache

--- greedo-layout-sample/src/main/java/com/fivehundredpx/greedo_layout_sample/Constants.java
+++ greedo-layout-sample/src/main/java/com/fivehundredpx/greedo_layout_sample/Constants.java
@@ -3,7 +3,7 @@ package com.fivehundredpx.greedo_layout_sample;
 import android.support.annotation.DrawableRes;
 
 /**
- * Created by JVillella on 16-02-24.
+ * Created by Julian Villella on 16-02-24.
  */
 public class Constants {
     public static final @DrawableRes int[] IMAGES = new int[] {
